1f03282ef43b36cf356c35fbd70790dfaa3020a0
voytenkov <voytenkov-80@yandex.ru>
2018-09-12 22:31:14 +1000

    0.21

--- js/map.js
+++ js/map.js
@@ -3,14 +3,17 @@ var modalMap = document.querySelector(".modal-map");
 var modalMapClose = modalMap.querySelector(".modal-close");
    
 mapLink.addEventListener("click", function (evt) {
+  "use strict";
   evt.preventDefault();
   modalMap.classList.add("modal-view");
 });
 modalMapClose.addEventListener("click", function (evt) {
+  "use strict";
   evt.preventDefault();
   modalMap.classList.remove("modal-view");
 });
 window.addEventListener("keydown", function (evt) {
+  "use strict";
   if (evt.keyCode === 27) {
     if (modalMap.classList.contains("modal-view")) {
       modalMap.classList.remove("modal-view");
